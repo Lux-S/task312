75afb97126b1aaf9da2fe9bc97782e35bef252ab
Lux-S <sutorminal@mail.ru>
2023-07-06 11:34:18 +0900

    Initial commit

---
+++ src/main/java/com/example/task312/model/User.java
@@ -0,0 +1,56 @@
+package com.example.task312.model;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "users")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private int age;
+    private String name;
+    private String email;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public User() {
+    }
+
+    public User(Long id, int age, String name, String email) {
+        this.id = id;
+        this.age = age;
+        this.name = name;
+        this.email = email;
+    }
+}
